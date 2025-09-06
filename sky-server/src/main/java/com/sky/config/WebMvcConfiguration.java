e20e4e52faadf0bf53b225fb5c24b22334b5cd87
sanm <sanmzhang@163.com>
2025-09-06 21:38:00 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,13 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,6 +17,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -65,5 +70,17 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始注册静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+/**
+ * 扩展消息转换器，用于自定义JSON消息转换器的配置
+ * @param converters 消息转换器列表，用于处理HTTP请求和响应的消息转换
+ */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("开始扩展消息转换器...");
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0, mappingJackson2HttpMessageConverter);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -16,8 +18,33 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+/**
+ * 向employee表中插入一条员工记录
+ * @param employee 包含员工完整信息的实体对象，包含以下字段：
+ *                 - name: 员工姓名
+ *                 - username: 员工用户名
+ *                 - password: 员工密码
+ *                 - phone: 员工电话号码
+ *                 - sex: 员工性别
+ *                 - idNumber: 员工身份证号
+ *                 - status: 员工状态
+ *                 - createTime: 创建时间
+ *                 - updateTime: 更新时间
+ *                 - createUser: 创建人
+ *                 - updateUser: 更新人
+ */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values " +
             "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
+
+
+/**
+ * 分页查询员工信息
+ * 该方法用于根据查询条件进行分页查询，返回一个包含员工信息的Page对象
+ *
+ * @param employeePageQueryDTO 员工分页查询条件数据传输对象，包含分页信息和查询条件
+ * @return Page<Employee> 返回一个分页对象，包含员工数据列表和分页信息
+ */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
